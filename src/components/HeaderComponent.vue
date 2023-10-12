426d44d238931f2ebbcecfb00ca8e67c28bbf2c3
chloe <chloe.tinsley@outlook.com>
2023-10-12 18:54:08 +0100

    making mobile nav close on selection

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,15 +1,14 @@
 <script setup>
-function toggleMenu() {
-  var el = document.getElementById('navItems')
-  el.classList.toggle('toggle')
-  el.classList.remove('hidden')
+import { onMounted, watch } from 'vue'
+import { useRoute } from 'vue-router'
 
-  var ell = document.getElementById('menu')
-  // console.log(ell)
-  ell.classList.toggle('cross')
-  ell.classList.toggle('burger')
+function toggleMenu() {
+  var navItems = document.getElementById('navItems')
+  navItems.classList.toggle('toggle')
+  navItems.classList.remove('hidden')
+
+  toggleNavIcon()
 }
-import { onMounted } from 'vue'
 
 onMounted(() => {
   setTimeout(function() {
@@ -17,11 +16,24 @@ onMounted(() => {
     el.classList.remove('preload')
   }, 5000)
 })
+
+function navClick() {
+  var navItems = document.getElementById('navItems')
+  navItems.classList.add('hidden')
+  navItems.classList.add('toggle')
+
+  toggleNavIcon()
+}
+
+function toggleNavIcon() {
+  var menu = document.getElementById('menu')
+  menu.classList.toggle('cross')
+  menu.classList.toggle('burger')
+}
 </script>
 
 <template>
   <header>
-    <!-- <div class="wrapper"> -->
     <nav class="header">
       <div class="top-content">
         <div class="logo-container">
@@ -34,23 +46,22 @@ onMounted(() => {
 
       <div id="navItems" class="nav-links toggle hidden">
         <div class="nav-items">
-          <RouterLink to="/about">About</RouterLink>
-          <RouterLink to="/about">Lessons</RouterLink>
-          <RouterLink to="/contact">Contact</RouterLink>
+          <RouterLink to="/about" @click="navClick()">About</RouterLink>
+          <RouterLink to="/about" @click="navClick()">Lessons</RouterLink>
+          <RouterLink to="/contact" @click="navClick()">Contact</RouterLink>
         </div>
       </div>
     </nav>
   </header>
-  <!-- </div> -->
 </template>
 
 <style scoped>
+/* .router-link-active {
+  text-decoration: underline !important;
+} */
 .header {
   background-color: #050a30;
-  /* display: flex;
-  align-items: stretch; */
   flex-direction: row;
-  /* height: 60px; */
   display: flex;
   align-items: stretch;
   justify-content: space-between;
@@ -61,19 +72,15 @@ onMounted(() => {
 }
 .logo-container {
   display: flex;
-  /* align-self: flex-start; */
-
   align-self: center;
   flex: 1;
 }
-
 .nav-links {
   display: flex;
   align-self: center;
   vertical-align: middle;
   height: 100%;
   align-items: center;
-  /* flex: 1; */
 }
 .nav-links a {
   margin-left: 35px;
@@ -82,20 +89,13 @@ onMounted(() => {
   font-size: 18px;
 }
 .burger {
-  /* background-position: right; */
   background-image: url('../assets/images/burger-menu.png');
-  /* background-repeat: no-repeat;
-  background-size: 30px; */
 }
 .cross {
-  /* background-position: right; */
   background-image: url('../assets/images/cross-menu2.png');
   background-size: 20px !important;
-  /* background-repeat: no-repeat;
-  background-size: 30px; */
 }
 .menu {
-  /* background-image: url('../assets/images/burger-menu.png');*/
   background-position: center;
   margin-left: auto;
   background-repeat: no-repeat;
@@ -105,9 +105,6 @@ onMounted(() => {
 }
 
 @media only screen and (max-width: 640px) {
-  /* .logo-container {
-    width: 75%;
-  } */
   header {
     max-height: 125px;
   }
@@ -118,8 +115,6 @@ onMounted(() => {
   .nav-items {
     display: flex;
     flex-direction: column;
-
-    /* background-size: cover; */
   }
   .top-content {
     padding: 30px 50px;
@@ -131,20 +126,14 @@ onMounted(() => {
   }
   .nav-items {
     position: relative;
-    /* z-index: 0; */
-    /* bottom: 180px;  */
   }
   .nav-items a {
     position: relative;
-    /* z-index: -1; */
   }
-
   .nav-links:not(.toggle) {
-    /* bottom: 0px; */
     animation: fadein 0.5s linear;
   }
   .nav-links.toggle {
-    /* bottom: 0px; */
     animation: fadeout 0.5s linear;
   }
   @keyframes fadein {
@@ -165,7 +154,6 @@ onMounted(() => {
   }
   .toggle {
     transform: translateY(-230px);
-    /* display: none !important; */
   }
   .top-content {
     display: flex;
@@ -173,37 +161,24 @@ onMounted(() => {
     justify-content: space-between;
     background-color: #050a30;
   }
-
   .nav-links {
     flex-direction: column;
     align-self: stretch;
-    /* padding-top: 30px; */
     z-index: 1;
     position: relative;
-
-    /* transition: 0.5s;
-    transform: translateY(-100%); */
   }
-
   .nav-items {
     text-align: center;
-
     width: 100%;
-
-    /* padding: 30px 40px 20px 40px; */
     background-color: #009fe3;
   }
   .burger,
   .cross {
     display: block;
   }
-  .nav-links a:first-of-type {
-    /* border-top: 0.5px solid #bbe3fa; */
-  }
   .nav-links a {
     padding: 25px 0;
     margin: 0;
-
     border-bottom: 0.5px solid #bbe3fa;
   }
 }
@@ -213,9 +188,6 @@ onMounted(() => {
   }
   .top-content {
     padding: 30px;
-  }
-  .nav-items {
-    /* padding: 30px 40px 20px 40px; */
   }
 }
 </style>
