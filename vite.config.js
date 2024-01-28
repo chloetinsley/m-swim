90fb655f7e532f07e4dd987c0b9508e0650a88d4
chloe <chloe.tinsley@outlook.com>
2024-01-28 20:07:28 +0000

    testing deploy

--- vite.config.js
+++ vite.config.js
@@ -13,5 +13,6 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  base: process.env.NODE_ENV === "production" ? "/m-swim/" : "/"
+  base: process.env.NODE_ENV === "/"
+  // base: process.env.NODE_ENV === "production" ? "/m-swim/" : "/"
 })
