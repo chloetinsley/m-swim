da6020d07afbae5d1b0b34c16e552c7792672b89
chloe <chloe.tinsley@outlook.com>
2024-02-18 20:34:08 +0000

    updating meta descriptions

--- src/router/index.js
+++ src/router/index.js
@@ -10,7 +10,7 @@ const router = createRouter({
       component: HomeView,
       meta: {
         title: 'M Swim | Swim School in Staffordshire',
-        description: "Nestled within the esteemed M Club Spa & Fitness in Staffordshire, we are more than just a swimming school; we are a beacon of water wisdom and wellness."
+        description: "Swimming lessons in Staffordshire based in M Club Spa & Fitness in Hanley and Newcastle-under-Lyme offering lessons for Babies and toddlers, Childrens beginner and Adults."
       }
     },
     {
@@ -19,7 +19,7 @@ const router = createRouter({
       component: () => import('../views/AboutView.vue'),
       meta: {
         title: 'M Swim | About',
-        description: "Swim school based in Staffordshire - Dive into the M Swim experience. Join us in celebrating the freedom, fitness, and fun that swimming brings to life. Whether you're here to conquer your fear of water, refine your strokes, or prepare for a triathlon, we're with you, every lap of the way."
+        description: "Swimming lessons in Staffordshire - Dive into the M Swim experience. Join us in celebrating the freedom, fitness, and fun that swimming brings to life. Whether you're here to conquer your fear of water, refine your strokes, or prepare for a triathlon, we're with you, every lap of the way."
       }
     },
     {
@@ -37,7 +37,7 @@ const router = createRouter({
       component: () => import('../views/LessonsView.vue'),
       meta: {
         title: 'M Swim | Swim Lessons in Staffordshire',
-        description: "Discover the Art of Swimming with M Swim. Our lessons are designed to be an exciting journey through water, guided by the renowned Swim England curriculum."
+        description: "Learn to swim with M Swim, an official STA partner and an accredited STA Swim Academy. Our lessons are designed to be an exciting journey through water."
       }
     }
   ],
